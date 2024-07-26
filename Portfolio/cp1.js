1e8d6c10b8b0f6fab1a12b24f34bbda6bf88ab7c
Vishal Deval <vishaldeval098@gmail.com>
2024-07-26 17:53:58 +0530

    upload portfolio

---
+++ Portfolio/cp1.js
@@ -0,0 +1,27 @@
+document.getElementsByClassName('instagram')[0].addEventListener("click",function(){
+    open('https://www.instagram.com/vdeval016/')
+})
+document.getElementsByClassName('linkden')[0].addEventListener('click', function () {
+    open('https://www.linkedin.com/in/vishal-deval-0a1283296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')
+})
+document.getElementsByClassName('github')[0].addEventListener('click', function () {
+    open('https://github.com/VishalDeval')
+})
+document.getElementsByClassName('telegram')[0].addEventListener('click', function () {
+    open('https://t.me/+cv6sThv7Cy4xY2Q9')
+})
+document.getElementsByClassName('page4child')[0].addEventListener('click',function(){
+    open('https://vishaldeval.github.io/Digital-Clock/')
+})
+document.getElementsByClassName('page4child')[1].addEventListener('click',function(){
+    open('https://vishaldeval.github.io/Codsoft/Calculator')
+})
+document.getElementsByClassName('page4child')[2].addEventListener('click',function(){
+    open('https://vishaldeval.github.io/To-Do-List/')
+})
+document.getElementsByClassName('hamburger')[0].addEventListener('click',function(){
+    document.getElementsByTagName('nav')[0].classList.toggle('expanded');
+})
+document.getElementsByClassName('page1')[0].addEventListener('click',function(){
+    document.getElementsByTagName('nav')[0].classList.toggle('expanded');
+})
